c59063b2bb2c21a35009d99e91580371fdefdc37
Bomin Fang <boomyf9876@gmail.com>
2018-11-21 22:23:25 -0500

    1

---
+++ src/processes/produce.c
@@ -0,0 +1,115 @@
+// Use this to see if a number has an integer square root
+#define EPS 1.E-7
+
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mqueue.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <math.h>
+
+double g_time[2];
+
+
+int main(int argc, char *argv[])
+{
+	int num;
+	int maxmsg;
+	int num_p;
+	int num_c;
+	int i,j,x;
+	struct timeval tv;
+
+	if (argc != 5) {
+		printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
+		exit(1);
+	}
+
+	num = atoi(argv[1]);	/* number of items to produce */
+	maxmsg = atoi(argv[2]); /* buffer size                */
+	num_p = atoi(argv[3]);  /* number of producers        */
+	num_c = atoi(argv[4]);  /* number of consumers        */
+
+	gettimeofday(&tv, NULL);
+	g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
+
+	mqd_t qdes;
+	mode_t mode = S_IRUSR | S_IWUSR;
+	struct mq_attr attr;
+	attr.mq_maxmsg  = maxmsg;
+	attr.mq_msgsize = sizeof(int);
+	attr.mq_flags   = 0;		/* a blocking queue  */
+
+	qdes = mq_open("/msg_q", O_RDWR | O_CREAT, 0777, &attr);
+
+	if (qdes == -1 ) {
+		perror("mq_open() failed");
+		exit(1);
+	}
+
+	for (i=0; i<num_p; i++) {
+		int producer = fork();
+		if (producer == 0) {
+			/*for (j=i;j<num;j+=num_p) {
+				if (i == num_p -1 && j+num_p>num) {
+					for(i = 0; i < num_c; i++){
+						int sign = -1;
+						mq_send(qdes, &sign, sizeof(int), NULL);
+					}
+				}
+			}*/
+		printf("producers starts \n");
+			for(j = 0; j< num; j++){
+				if(j % num_p == i){
+					mq_send(qdes, &i, sizeof(int), NULL);
+					printf("sending %d \n", i);
+				}
+			}
+
+			exit(0);
+		}
+	}
+
+	for (i=0; i<num_c; i++) {
+		int consumer = fork();
+		if (consumer == 0) {
+			double receiver;
+			// for(detector = mq_receive(qdes, &receiver, sizeof(int), NULL);receiver != -1;){
+			// 	if (detector == -1) {
+			// 		perror("mq_receive() failed");
+			// 	}
+			// 	if(sqrt((double)receiver) - floor(sqrt((double)receiver)) == 0){
+			// 		printf("%d       %d        %d", i, receiver, sqrt((double)receiver));
+			// 	}
+			// }
+			while(1){
+				mq_receive(qdes, &receiver, sizeof(int), NULL);
+				if(sqrt((double)receiver) - floor(sqrt((double)receiver)) == 0){
+					printf("%d       %d        %d", i, receiver, sqrt((double)receiver));
+				}
+			}
+			exit(0);
+		}
+	}
+
+    gettimeofday(&tv, NULL);
+    g_time[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
+
+    printf("System execution time: %.6lf seconds\n", \
+            g_time[1] - g_time[0]);
+	exit(0);
+}
+
+
+
+// waitpid(int pid, NULL, 0);//回收
+// producer是否全回收
+// mq_getattr(mqid, &testAttr)//testAttr is struct mq_attr
+// attr.mq_curmsgs//剩余数量
+// kill(pid, 9);
---
+++ src/threads/produce.c
@@ -0,0 +1,169 @@
+// Use this to see if a number has an integer square root
+#define EPS 1.E-7
+
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mqueue.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <math.h>
+#include <semaphore.h>
+#include <pthread.h>
+
+double g_time[2];
+
+typedef struct {
+	int * buffer;
+	int start;
+	int end;
+	int size;
+} circuler_arr;
+
+typedef struct {
+	int size;
+	int index;
+	int num_producer;
+	circuler_arr * buffer_ptr;
+} container_p;
+
+
+int count = 0;
+sem_t e;
+sem_t f;
+sem_t a;
+
+void* produce( container_p param ) {
+	printf('p1');
+	for (int i = param.index; i < param.size; i += param.num_producer) {
+		printf('p2');
+        sem_wait( &f );
+        sem_wait( &a );
+		int newend = param.buffer_ptr->end + 1;
+		if (newend == param.buffer_ptr->size) {
+			param.buffer_ptr->buffer[0] = i;
+			param.buffer_ptr->end = 0;
+		}
+		param.buffer_ptr->buffer[newend] = i;
+		param.buffer_ptr->end = newend;
+		sem_post( &e );
+        sem_post( &a );
+	}
+    printf("A producer has finished.\n");
+	pthread_exit(0);
+}
+
+void* consume( container_p param ) {
+	printf('c1');
+    container_p upper =  param ;
+	while (count < upper.size) {
+		printf('c2');
+        sem_wait( &e );
+        sem_wait( &a );
+		int popnum, newstart = 0;
+		if (param.buffer_ptr->start != param.buffer_ptr->end) {
+			newstart = param.buffer_ptr->start + 1;
+			popnum = param.buffer_ptr->buffer[param.buffer_ptr->start];
+			param.buffer_ptr->buffer[param.buffer_ptr->start] = -1;
+			param.buffer_ptr->start = (newstart == param.buffer_ptr->size) ? 0 : newstart;
+		} else {
+			pthread_exit(0);
+		}
+		count++;
+		if (count >= upper.size - upper.num_producer) {
+			pthread_exit(0);
+		}
+		printf("$d       $d       ", param.index, popnum);
+		if(sqrt((double)popnum) - floor(sqrt((double)popnum)) == 0){
+			printf("$d       $d        $d", param.index, popnum, sqrt((double)popnum));
+		}
+		sem_post( &f );
+        sem_post( &a );
+	}
+    printf("A consumer has finished.\n");
+	//pthread_exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+	int num;
+	int maxmsg;
+	int num_p;
+	int num_c;
+	int i;
+	struct timeval tv;
+	
+
+	if (argc != 5) {
+		printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
+		exit(1);
+	}
+
+	num = atoi(argv[1]);	/* number of items to produce */
+	maxmsg = atoi(argv[2]); /* buffer size                */
+	num_p = atoi(argv[3]);  /* number of producers        */
+	num_c = atoi(argv[4]);  /* number of consumers        */
+
+	pthread_t tid_p[num_p];
+	pthread_t tid_c[num_c];
+
+
+	gettimeofday(&tv, NULL);
+	g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
+
+	circuler_arr buff;
+	int buffer[maxmsg];
+	buff.buffer = buffer;
+	buff.start = 0;
+	buff.end = 0;
+	buff.size = maxmsg;
+
+	sem_init( &a, 0, 1 );
+	sem_init( &f, 0, maxmsg);
+	sem_init( &e, 0, 0);
+
+	container_p p;
+
+	for ( int i = 0; i < num_p; ++i ) {
+		p.size = num;
+		p.index = i;
+		p.num_producer = num_p;
+		p.buffer_ptr = &buff;
+		pthread_create(&tid_p[i], NULL, produce, &p);
+    }
+
+	for ( int i = 0; i < num_c; ++i ) {
+		p.size = num;
+		p.index = i;
+		p.num_producer = num_c;
+		p.buffer_ptr = &buff;
+		pthread_create(&tid_c[i], NULL, consume, &p);
+    }
+
+
+	for ( int j = 0; j < num_p; ++j ) {
+        pthread_join( tid_p[j], NULL );
+    }
+
+	for ( int j = 0; j < num_c; ++j ) {
+        pthread_join( tid_c[j], NULL );
+    }
+	
+    sem_destroy( &a );
+	sem_destroy( &f );
+	sem_destroy( &e );
+
+    gettimeofday(&tv, NULL);
+    g_time[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
+
+    printf("System execution time: %.6lf seconds\n", \
+            g_time[1] - g_time[0]);
+	exit(0);
+}
+
+
