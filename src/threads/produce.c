0f4e01031d040dad69e55e6f28bddb6bd1eba39b
Bomin Fang <boomyf9876@gmail.com>
2018-11-22 19:53:55 -0500

    1

--- src/threads/produce.c
+++ src/threads/produce.c
@@ -19,39 +19,31 @@
 double g_time[2];
 
 typedef struct {
-	int * buffer;
-	int start;
+	int *buffer;
 	int end;
-	int size;
 } circuler_arr;
 
 typedef struct {
 	int size;
 	int index;
 	int num_producer;
-	circuler_arr * buffer_ptr;
 } container_p;
 
+circuler_arr arr;
 
-int count = 0;
 sem_t e;
 sem_t f;
 sem_t a;
 
-void* produce( container_p *param ) {
-	container_p upper = *param;
+void* produce( container_p param ) {
+	container_p upper = param;
 	printf('p1');
 	for (int i = upper.index; i < upper.size; i += upper.num_producer) {
 		printf('p2');
         sem_wait( &f );
         sem_wait( &a );
-		int newend = param->buffer_ptr->end + 1;
-		if (newend == param->buffer_ptr->size) {
-			param->buffer_ptr->buffer[0] = i;
-			param->buffer_ptr->end = 0;
-		}
-		param->buffer_ptr->buffer[newend] = i;
-		param->buffer_ptr->end = newend;
+			memset(arr.buffer + arr.end, i, sizeof(int));
+			arr.end++;
 		sem_post( &e );
         sem_post( &a );
 	}
@@ -59,30 +51,25 @@ void* produce( container_p *param ) {
 	pthread_exit(0);
 }
 
-void* consume( container_p *param ) {
-	container_p upper = *param;
+void* consume( container_p param ) {
+	container_p upper = param;
+	int count, popnum, newstart  = 0;
 	printf('c1');
 	while (count < upper.size) {
 		printf('c2');
         sem_wait( &e );
         sem_wait( &a );
-		int popnum, newstart = 0;
-		if (param->buffer_ptr->start != param->buffer_ptr->end) {
-			newstart = param->buffer_ptr->start + 1;
-			popnum = param->buffer_ptr->buffer[param->buffer_ptr->start];
-			param->buffer_ptr->buffer[param->buffer_ptr->start] = -1;
-			param->buffer_ptr->start = (newstart == param->buffer_ptr->size) ? 0 : newstart;
-		} else {
-			pthread_exit(0);
-		}
-		count++;
-		if (count >= upper.size - upper.num_producer) {
-			pthread_exit(0);
-		}
-		printf("%d       %d       ", upper.index, popnum);
-		if(sqrt((double)popnum) - floor(sqrt((double)popnum)) == 0){
-			printf("%d       %d        %d", upper.index, popnum, (int)sqrt((double)popnum));
-		}
+			popnum = arr.buffer[arr.end];
+			memset(arr.buffer + arr.end, 0, sizeof(int));
+			arr.end--;
+			count++;
+			if (count >= upper.size - upper.num_producer) {
+				pthread_exit(0);
+			}
+			printf("%d       %d       ", upper.index, popnum);
+			if(sqrt((double)popnum) - floor(sqrt((double)popnum)) == 0){
+				printf("%d       %d        %d", upper.index, popnum, (int)sqrt((double)popnum));
+			}
 		sem_post( &f );
         sem_post( &a );
 	}
@@ -92,6 +79,7 @@ void* consume( container_p *param ) {
 
 int main(int argc, char *argv[])
 {
+	printf("I am here 4");
 	int num;
 	int maxmsg;
 	int num_p;
@@ -103,7 +91,7 @@ int main(int argc, char *argv[])
 		printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
 		exit(1);
 	}
-
+	printf("I am here1");
 	num = atoi(argv[1]);	/* number of items to produce */
 	maxmsg = atoi(argv[2]); /* buffer size                */
 	num_p = atoi(argv[3]);  /* number of producers        */
@@ -116,38 +104,31 @@ int main(int argc, char *argv[])
 	gettimeofday(&tv, NULL);
 	g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
 
-	int buffer[maxmsg];
-	circuler_arr buff = {
-		buffer,
-		0,
-		0,
-		maxmsg
-	};
-	//buff.buffer = buffer;
-	//buff.start = 0;
-	//buff.end = 0;
-	//buff.size = maxmsg;
+	memset(arr.buffer, 0, sizeof(int)*maxmsg);
+	arr.end = 0;
 
 	sem_init( &a, 0, 1 );
 	sem_init( &f, 0, maxmsg);
 	sem_init( &e, 0, 0);
 
 	container_p p;
-
+	printf("I am here2");
 	p.size = num;
-	p.buffer_ptr = &buff;
-	printf("1\n");
 	for ( int i = 0; i < num_p; ++i ) {
 		p.index = i;
 		p.num_producer = num_p;
+		printf("I am here3");
 		pthread_create(&tid_p[i], NULL, produce, &p);
     }
+	printf("I am here4");
 
 	for ( int j = 0; j < num_c; ++j ) {
 		p.index = i;
 		p.num_producer = num_c;
+		printf("I am here5");
 		pthread_create(&tid_c[i], NULL, consume, &p);
     }
+	printf("I am here6");
 
 
 	for ( int k = 0; k < num_p; ++k ) {
